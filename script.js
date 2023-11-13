d96505b0c6c1ef5ec0dac7d361ea9e292eef1e55
SyuusukeOura <82437524+SyuusukeOura@users.noreply.github.com>
2023-11-13 15:19:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// JavaScript
+
+var map = L.map('map').setView([33.52277040125248, 130.46915889312515], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([33.530073091131605, 130.47303414811546], { icon: whiteIcon }).addTo(map).bindPopup('こんにちは！<br><img src="images/img01.png" alt="img">');
+
+//アイコン
+const whiteIcon = L.icon({
+    iconUrl: 'images/ico.png',
+    shadowUrl: 'images/ico_shadow.png',
+  
+  iconSize:     [40, 40], // size of the icon
+  shadowSize:   [40, 40], // size of the shadow
+  iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
+  shadowAnchor: [20, 40],  // the same for the shadow
+  popupAnchor:  [0, -42] // point from which the popup should open relative to the iconAnchor
+  });
