97eebb665e99214955d1f6d97a350fbfc713e8ea
SyuusukeOura <82437524+SyuusukeOura@users.noreply.github.com>
2023-11-20 14:51:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,16 +6,56 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([33.530073091131605, 130.47303414811546], { icon: whiteIcon }).addTo(map).bindPopup('こんにちは！<br><img src="images/img01.png" alt="img">');
 
-//アイコン
-const whiteIcon = L.icon({
-    iconUrl: 'images/ico.png',
-    shadowUrl: 'images/ico_shadow.png',
-  
-  iconSize:     [40, 40], // size of the icon
-  shadowSize:   [40, 40], // size of the shadow
-  iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
-  shadowAnchor: [20, 40],  // the same for the shadow
-  popupAnchor:  [0, -42] // point from which the popup should open relative to the iconAnchor
+  //複数アイコンをまとめて定義
+const circleIcon = L.Icon.extend({
+    options: {
+      shadowUrl: 'images/ico_shadow.png',
+      iconSize: [40, 40],
+      shadowSize: [40, 40],
+      iconAnchor: [20, 40],
+      shadowAnchor: [20, 40],
+      popupAnchor: [0, -42]
+    }
   });
+  
+  const whiteIcon = new circleIcon({ iconUrl: 'images/ico.png' }),
+    pinkIcon = new circleIcon({ iconUrl: 'images/ico_pink.png' });
+    blueIcon = new circleIcon({ iconUrl: 'images/ico_blue.png' });
+
+
+  L.marker([33.530073091131605, 130.47303414811546], { icon: whiteIcon }).addTo(map).bindPopup('春日公園');
+  L.marker([33.53552283217252, 130.4685127098311], { icon: pinkIcon }).addTo(map).bindPopup('春日駅');
+  L.marker([33.511867415603874, 130.45390144805646], { icon: blueIcon }).addTo(map).bindPopup('白水大池公園');
+
+  const circle = L.circle([33.530073091131605, 130.47303414811546], {
+    color: 'red', //円の輪郭線の色
+    fillColor: '#f03', //円の塗りつぶしの色
+    fillOpacity: 0.3, //塗りつぶしの不透明度
+    radius: 1000 //半径、メートルで指定
+  }).addTo(map);
+
+  circle.bindPopup("半径1kmの範囲").openPopup();
+
+  // 多角形の表示
+const polygon = L.polygon([
+    [33.518977778407326, 130.4365017931888],
+    [33.527077492343345, 130.4553625786904],
+    [33.51628001352016, 130.44805783695767]
+  ], {
+    color: 'blue',
+    fillColor: 'blue',
+    fillOpacity: 0.3
+  }).addTo(map);
+
+  // クリック位置の緯度経度表示
+const popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("ここは" + e.latlng.toString() + "です")
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
